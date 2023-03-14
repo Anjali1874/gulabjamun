e03d1bcfe1b1b91b08f3ff75352d628e02cf4d91
thundersp <thundersp20504@gmail.com>
2023-03-14 19:31:55 +0530

    ok

--- thunderworks/index.js
+++ thunderworks/index.js
@@ -5,6 +5,3 @@ document.getElementById("infoch1").addEventListener("click", function () {
   const myElement = document.getElementById("infoch1");
   myElement.style.color = "red";
 });
-document.getElementById('login').addEventListener('click',function(){
-    alert(document.getElementsByClassName('flexhead item3'));
-});
